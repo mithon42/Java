cdca11585b35acdedd93ab584e83275b3d8756ae
mithon42 <mithonphilip@gmail.com>
2022-06-30 22:40:33 +0545

    Testing Message

---
+++ text.java
@@ -0,0 +1,10 @@
+
+public class text {
+  public static void main(String[] args) {
+    System.out.println("Testing Message");
+    thanksMessage("Thank You.");
+  }
+  public static void thanksMessage(String message) {
+    System.out.println("message");
+  }
+}
