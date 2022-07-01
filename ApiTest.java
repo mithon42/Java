0b62ca481f0673096bbfa49e7eeaafd3150f3299
mithon42 <mithonphilip@gmail.com>
2022-07-01 09:51:57 +0545

    Added new mathord

--- ApiTest.java
+++ ApiTest.java
@@ -1,5 +1,11 @@
 public class ApiTest {
   public static void main(String[] args) {
     System.out.println(Math.sqrt(16));
+
+    max(args);
+  }
+
+  public static void max(String[] args) {
+    System.out.println(Math.max(120, 100));
   }
 }
